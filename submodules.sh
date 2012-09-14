b1d3e8ad9945c4cb5feb2269aad75180066bade2
Kyle Terry <kyle@kyleterry.com>
2012-09-14 14:01:33 -0700

    Added things.

---
+++ submodules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule init
+git submodule foreach git submodule update
