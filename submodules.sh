61a5a66c03bfc45b4fb21627e1a3ad2999bc1cc8
Kyle Terry <kyle@kyleterry.com>
2014-05-05 14:03:20 -0700

    Submodules update

--- submodules.sh
+++ submodules.sh
@@ -4,5 +4,7 @@ git submodule sync
 git submodule init
 git submodule update
 git submodule foreach git pull origin master
+echo "Initing submodules"
 git submodule foreach git submodule init
+echo "Updating submodules"
 git submodule foreach git submodule update
