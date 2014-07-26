34ed02102968588206e08241cc4fb0ad5bc996d1
Kyle Terry <kyle@kyleterry.com>
2014-07-26 11:09:23 -0700

    Added layouts

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -3,7 +3,12 @@ import XMonad.Util.Run
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.SimpleFloat
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Tabbed
 import Control.Monad
 import System.IO
 import qualified XMonad.StackSet as W
@@ -13,9 +18,12 @@ myModMask = mod4Mask
 myWorkspaces = ["main", "web", "code", "games", "docs"] ++ map show [6..9]
 myManagehook = composeAll . concat $
     [
-        [ className =? b --> viewShift "web" | b <- myClassWebShifts ],
-        [ className =? i --> doFloat | i <- myClassFloats ++ myClassGames ],
-        [ className =? g --> viewShift "games" | g <- myClassGames ]
+        [ className =? b  --> viewShift "web" | b <- myClassWebShifts ],
+        [ className =? i  --> doFloat | i <- myClassFloats ++ myClassGames ],
+        [ className =? g  --> viewShift "games" | g <- myClassGames ],
+        [ className =? ig --> doIgnore | ig <- myClassGames ],
+        [ isDialog        --> doCenterFloat ],
+        [ isFullscreen    --> doFullFloat ]
     ]
     where
         viewShift = doF . liftM2 (.) W.greedyView W.shift
@@ -30,6 +38,21 @@ myLogHook h = dynamicLogWithPP $ xmobarPP
                                     , ppUrgent = xmobarColor myUrgentWSColor ""
                                         . wrap myUrgentWSLeft myUrgentWSRight
                                     }
+
+-- Layouts
+defaultLayouts = tiled ||| Mirror tiled ||| simpleFloat ||| Full ||| simpleTabbed
+    where
+        tiled = Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 1/2
+        delta = 3/100
+
+gamesLayout = noBorders $ Full
+
+myLayoutHook = avoidStruts
+             $ onWorkspace "games" gamesLayout
+             $ defaultLayouts
+
 myCurrentWSColor = "#e6744c" -- color of active workspace
 myVisibleWSColor = "#c185a7" -- color of inactive workspace
 myUrgentWSColor = "#cc0000" -- color of workspace with 'urgent' window
@@ -50,6 +73,6 @@ main = do
         manageHook  = manageDocks <+> myManagehook <+> manageHook defaultConfig,
         modMask     = myModMask,
         logHook     = myLogHook xmproc,
-        layoutHook = avoidStruts  $  layoutHook defaultConfig,
+        layoutHook  = myLayoutHook,
         borderWidth = 1
     }
