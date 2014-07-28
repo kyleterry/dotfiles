ae6680e39363d5d1497b1a41d321776a03996c13
Kyle Terry <kyle@kyleterry.com>
2014-07-27 22:32:14 -0700

    Added an array to map keys

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -11,7 +11,9 @@ import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Tabbed
 import Control.Monad
 import System.IO
+import Graphics.X11.ExtraTypes.XF86
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
 myTerminal = "urxvt"
 myModMask = mod4Mask
@@ -47,12 +49,13 @@ defaultLayouts = tiled ||| Mirror tiled ||| simpleFloat ||| Full ||| simpleTabbe
         ratio = 1/2
         delta = 3/100
 
-gamesLayout = noBorders $ Full
-
 myLayoutHook = avoidStruts
-             $ onWorkspace "games" gamesLayout
-             $ defaultLayouts
+    $ onWorkspace "games" gamesLayout
+    $ defaultLayouts
+    where
+        gamesLayout = noBorders $ Full
 
+-- Workspace colors
 myCurrentWSColor = "#e6744c" -- color of active workspace
 myVisibleWSColor = "#c185a7" -- color of inactive workspace
 myUrgentWSColor = "#cc0000" -- color of workspace with 'urgent' window
@@ -61,6 +64,20 @@ myUrgentWSRight = "}"
 
 startupWorkspace = "code"
 
+--myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+--myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+keysToAdd x =
+    [ ((modMask x .|. controlMask, xK_l), spawn "slock")
+    , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
+    , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
+    , ((0, xF86XK_AudioNext), spawn "ncmpcpp next")
+    , ((0, xF86XK_AudioPrev), spawn "ncmpcpp prev")
+    , ((0, xF86XK_AudioPlay), spawn "ncmpcpp toggle")
+    , ((0, xF86XK_AudioStop), spawn "ncmpcpp stop")
+    , ((0, xK_Print), spawn "scrot '%Y-%m-%d_$wx$h.png'")
+    ]
+myKeys x = M.union (keys defaultConfig x) (M.fromList (keysToAdd x))
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
@@ -70,5 +87,6 @@ main = do
         modMask     = myModMask,
         logHook     = myLogHook xmproc,
         layoutHook  = myLayoutHook,
+        keys        = myKeys,
         borderWidth = 1
     }
