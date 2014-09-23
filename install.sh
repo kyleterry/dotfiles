1f027925d8d5405691ff877de1e115afe7f1b285
Kyle Terry <kyle@kyleterry.com>
2014-09-22 18:34:48 -0700

    Added quotes to prevent globbing

--- install.sh
+++ install.sh
@@ -14,64 +14,64 @@ if [[ ! ${MANIFEST} ]]; then
     exit 1
 fi
 
-mkdir -p ${ROAMER_BACKUP_DIR}
+mkdir -p "${ROAMER_BACKUP_DIR}"
 
 echo "==> Reading manifest and linking files..."
-for item in $(cat ${MANIFEST}); do
-    set -- $(echo ${item} | tr '=' ' ')
+for item in $(cat "${MANIFEST}"); do
+    set -- $(echo "${item}" | tr '=' ' ')
     file_source=${BASE_DIR}/${1}
     file_destination=${2}
     file_destination_full=${HOME}/${file_destination}
     echo "==> Backing up ${file_destination_full}..."
     if [[ -f ${file_destination_full} ]] || [[ -d ${file_destination_full} ]]; then
         if [[ ! -L ${file_destination_full} ]]; then
-            mv ${file_destination_full} ${ROAMER_BACKUP_DIR}/${file_destination}.bak-${DATE}
+            mv "${file_destination_full}" "${ROAMER_BACKUP_DIR}/${file_destination}.bak-${DATE}"
         else
             echo "==> Not a real file!"
             echo "====> Removing old link..."
-            rm ${file_destination_full}
+            rm "${file_destination_full}"
         fi
     fi
     echo "==> Linking ${file_source} -> ${file_destination_full}"
-    ln -s ${file_source} ${file_destination_full}
+    ln -s "${file_source}" "${file_destination_full}"
 done
 
 # Make sure ${HOME}/.conf.d exists
 if [[ ! -f ${HOME}/.conf.d ]]; then
     echo "==> Creating ${HOME}/.conf.d"
     echo "====> You can symlink scripts to here from ${BASE_DIR}/bash_conf.d"
-    mkdir -p ${HOME}/.conf.d
+    mkdir -p "${HOME}/.conf.d"
 fi
 
 # Make sure ${HOME}/.vimtmp exists
 if [[ ! -f ${HOME}/.vimtmp ]]; then
     echo "==> Creating ${HOME}/.vimtmp"
-    mkdir -p ${HOME}/.vimtmp
+    mkdir -p "${HOME}/.vimtmp"
 fi
 
 # Make sure ${HOME}/bin exists
 if [[ ! -f ${HOME}/bin ]]; then
     echo "==> Creating ${HOME}/bin"
-    cp -r ${BASE_DIR}/bin ${HOME}
+    cp -r "${BASE_DIR}/bin ${HOME}"
 fi
 
 if [[ -d ${BASE_DIR}/install_hooks.d ]]; then
     echo "==> Sourcing install hooks"
     for item in ${BASE_DIR}/install_hooks.d/*; do
         if [[ -x ${item} ]]; then
-            . ${item}
+            source "${item}"
         fi
     done
 fi
 
 echo "==> Updating submodules..."
-${BASE_DIR}/submodules.sh | grep 'Entering' | awk '{print $2}'
+"${BASE_DIR}"/submodules.sh | grep 'Entering' | awk '{print $2}'
 
 if [ -f /etc/debian_version ]; then
     echo "==> Sniffed Debian; Installing packages"
     sudo apt-get update
-    sudo apt-get install -y ${APT_PACKAGES[@]}
-    sudo pip install ${PIP_PACKAGES[@]}
+    sudo apt-get install -y "${APT_PACKAGES[@]}"
+    sudo pip install "${PIP_PACKAGES[@]}"
     echo "==> Setting wallpaper"
-    feh --bg-scale ${HOME}/.wallpaper/${INITIAL_WP}
+    feh --bg-scale "${HOME}/.wallpaper/${INITIAL_WP}"
 fi
