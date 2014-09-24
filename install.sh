16ae338fc2cfb54b9b7e8f413cd34955b568adc7
Kyle Terry <kyle@kyleterry.com>
2014-09-23 19:52:35 -0700

    Convention

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROAMER_BACKUP_DIR=${HOME}/.roamer_backups
-DATE=$(date +%s)
-INITIAL_WP=RCkQyvn.jpg
-APT_PACKAGES=(git mercurial golang python-pip build-essential htop dzen2 tmux
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+roamer_backup_dir=${HOME}/.roamer_backups
+_date=$(date +%s)
+initial_wp=RCkQyvn.jpg
+declare -a apt_packages=(git mercurial golang python-pip build-essential htop dzen2 tmux
     feh xautolock xmonad xmobar xorg feh)
-PIP_PACKAGES=(virtualenvwrapper udiskie beets)
-MANIFEST=${1}
+declare -a pip_packages=(virtualenvwrapper udiskie beets)
+manifest=${1}
 
-if [[ ! ${MANIFEST} ]]; then
+if [[ ! ${manifest} ]]; then
     echo "usage: ${0} manifests/manifest.in"
     exit 1
 fi
 
-mkdir -p "${ROAMER_BACKUP_DIR}"
+mkdir -p "${roamer_backup_dir}"
 
 echo "==> Reading manifest and linking files..."
 while IFS="=" read -r file_source file_destination
 do
-    file_source_full=${BASE_DIR}/${file_source}
+    file_source_full=${base_dir}/${file_source}
     file_destination_full=${HOME}/${file_destination}
     echo "==> Backing up ${file_destination_full}..."
     if [[ -f ${file_destination_full} ]] || [[ -d ${file_destination_full} ]]; then
         if [[ ! -L ${file_destination_full} ]]; then
-            mv "${file_destination_full}" "${ROAMER_BACKUP_DIR}/${file_destination}.bak-${DATE}"
+            mv "${file_destination_full}" "${roamer_backup_dir}/${file_destination}.bak-${_date}"
         else
             echo "==> Not a real file!"
             echo "====> Removing old link..."
@@ -33,12 +33,12 @@ do
     fi
     echo "==> Linking ${file_source} -> ${file_destination_full}"
     ln -s "${file_source}" "${file_destination_full}"
-done < "${MANIFEST}"
+done < "${manifest}"
 
 # Make sure ${HOME}/.conf.d exists
 if [[ ! -f ${HOME}/.conf.d ]]; then
     echo "==> Creating ${HOME}/.conf.d"
-    echo "====> You can symlink scripts to here from ${BASE_DIR}/bash_conf.d"
+    echo "====> You can symlink scripts to here from ${base_dir}/bash_conf.d"
     mkdir -p "${HOME}/.conf.d"
 fi
 
@@ -51,12 +51,12 @@ fi
 # Make sure ${HOME}/bin exists
 if [[ ! -f ${HOME}/bin ]]; then
     echo "==> Creating ${HOME}/bin"
-    cp -r "${BASE_DIR}/bin ${HOME}"
+    cp -r "${base_dir}/bin ${HOME}"
 fi
 
-if [[ -d ${BASE_DIR}/install_hooks.d ]]; then
+if [[ -d ${base_dir}/install_hooks.d ]]; then
     echo "==> Sourcing install hooks"
-    for item in ${BASE_DIR}/install_hooks.d/*; do
+    for item in ${base_dir}/install_hooks.d/*; do
         if [[ -x ${item} ]]; then
             source "${item}"
         fi
@@ -64,13 +64,13 @@ if [[ -d ${BASE_DIR}/install_hooks.d ]]; then
 fi
 
 echo "==> Updating submodules..."
-"${BASE_DIR}"/submodules.sh | grep 'Entering' | awk '{print $2}'
+"${base_dir}"/submodules.sh | grep 'Entering' | awk '{print $2}'
 
-if [ -f /etc/debian_version ]; then
+if [[ -f /etc/debian_version ]]; then
     echo "==> Sniffed Debian; Installing packages"
     sudo apt-get update
-    sudo apt-get install -y "${APT_PACKAGES[@]}"
-    sudo pip install "${PIP_PACKAGES[@]}"
+    sudo apt-get install -y "${apt_packages[@]}"
+    sudo pip install "${pip_packages[@]}"
     echo "==> Setting wallpaper"
-    feh --bg-scale "${HOME}/.wallpaper/${INITIAL_WP}"
+    feh --bg-scale "${HOME}/.wallpaper/${initial_wp}"
 fi
